a286c913de25b52f21951b4669d47c5c21f007ac
S Vijaya Lakshmi <vijayalakshmi.svl.0017@gmail.com>
2025-05-25 22:31:57 +0530

    added UC2-Validation

--- src/main/java/Spring/Address/Book/exception/GlobalExceptionHandler.java
+++ src/main/java/Spring/Address/Book/exception/GlobalExceptionHandler.java
@@ -3,23 +3,22 @@ package Spring.Address.Book.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice
-public class ValidationExceptionHandler {
+@ControllerAdvice
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error -> {
-            errors.put(error.getField(), error.getDefaultMessage());
-        });
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
