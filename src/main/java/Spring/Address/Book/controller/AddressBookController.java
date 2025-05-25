1f3455c42ef47fb742697838f2ecf86461001699
S Vijaya Lakshmi <vijayalakshmi.svl.0017@gmail.com>
2025-05-25 20:51:41 +0530

    UC2-Setup added

--- src/main/java/Spring/Address/Book/controller/AddressBookController.java
+++ src/main/java/Spring/Address/Book/controller/AddressBookController.java
@@ -1,13 +1,45 @@
 package Spring.Address.Book.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import Spring.Address.Book.utility.AddressBook;
+import Spring.Address.Book.utility.AddressBookUtility;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
+@RequestMapping("/addressbook")
 public class AddressBookController {
 
-    @GetMapping("/addressbook")
-    public String getWelcomeMessage() {
-        return "Welcome to the Address Book App!";
+    @GetMapping
+    public ResponseEntity<List<AddressBook>> getAll() {
+        return ResponseEntity.ok(AddressBookUtility.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AddressBook> getById(@PathVariable Long id) {
+        return AddressBookUtility.getById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<AddressBook> create(@RequestBody AddressBook addressBook) {
+        return new ResponseEntity<>(AddressBookUtility.add(addressBook), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AddressBook> update(@PathVariable Long id, @RequestBody AddressBook updated) {
+        return AddressBookUtility.update(id, updated)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        if (AddressBookUtility.delete(id)) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
---
+++ src/main/java/Spring/Address/Book/utility/AddressBook.java
@@ -0,0 +1,12 @@
+package Spring.Address.Book.utility;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressBook {
+    private Long id;
+    private String name;
+    private String email;
+}
