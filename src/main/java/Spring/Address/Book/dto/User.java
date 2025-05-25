41a153ba0f1937d914ca50bf15f1a9906cd7838c
S Vijaya Lakshmi <vijayalakshmi.svl.0017@gmail.com>
2025-05-25 21:59:26 +0530

    Added UC1-ApplicationSettings

--- src/main/java/Spring/Address/Book/dto/User.java
+++ src/main/java/Spring/Address/Book/dto/User.java
@@ -1,49 +1,15 @@
 package Spring.Address.Book.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private Long id;
     private String name;
     private String address;
     private String phone;
-
-    public User() {}
-
-    public User(Long id, String name, String address, String phone) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
